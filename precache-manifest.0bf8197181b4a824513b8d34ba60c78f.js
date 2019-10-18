b01e042ee9a9a6db07b43f8d51129d5566cbc968
abdugh <abderrahmen.gharsallah@yahoo.com>
2019-10-18 14:30:16 +0100

    Updates

---
+++ precache-manifest.0bf8197181b4a824513b8d34ba60c78f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2346358c663b2f18832bdce03d0c9638",
+    "url": "/loi-finance/index.html"
+  },
+  {
+    "revision": "630cf103ab3c47d2f1e5",
+    "url": "/loi-finance/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "823b503353b10d3b3025",
+    "url": "/loi-finance/static/js/2.c108a066.chunk.js"
+  },
+  {
+    "revision": "630cf103ab3c47d2f1e5",
+    "url": "/loi-finance/static/js/main.70f734fa.chunk.js"
+  },
+  {
+    "revision": "39af46295c54bddc69e0",
+    "url": "/loi-finance/static/js/runtime-main.20de03cd.js"
+  }
+]);
